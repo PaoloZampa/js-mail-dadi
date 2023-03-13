874e1f144133b17484409baa04e056955b2604b9
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-03-13 16:04:43 +0100

    intial commit e forse ultimo. scusate non funzionava il tasto commit

--- dice-js/assets/js/main.js
+++ dice-js/assets/js/main.js
@@ -1,17 +1,44 @@
-/* Mail
-Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
-Mail Bonus
-Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.
-Gioco dei dadi
+
+/* Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 Prima di partire a scrivere codice poniamoci qualche domanda:
 Che ci sia un array da qualche parte?
-Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-Consigli del giorno:
-scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-si ma noi cosa vogliamo fare?
-torniamo a scrivere in italiano
-proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
-Buon divertimento e confermate lettura come al solito  */
+Se dobbiamo confrontare qualcosa che "cosa" ci serve? */
 
+
+//faccio costanti
+const player1 = "PLAYER 1";
+const player2 = "PLAYER 2";
+const result_1 = document.getElementById('result1')
+const result_2 = document.getElementById('result2')
+const rollTheDice = document.getElementById('check_button');
+
+//faccio funzione
+function rollDice() {
+
+    //genero numeri random
+    let random1 = Math.floor(Math.random() * 6) + 1;
+    let random2 = Math.floor(Math.random() * 6) + 1;
+    console.log(random1, random2)
+
+    result_1.innerHTML = random1
+    result_2.innerHTML = random2
+
+    if (random1 === random2) {
+        document.querySelector("h1").innerHTML = "PAREGGIO SCHIAPPE!";
+    }
+
+    else if (random1 < random2) {
+        document.querySelector("h1").innerHTML
+            = (player2 + " VINCE PORCODDUE!");
+    }
+
+    else {
+        document.querySelector("h1").innerHTML
+            = (player1 + " VINCE PORCODDUE!");
+    }
+
+}
+
+//pulsante per iniziare un random
+check_button.addEventListener('click', rollDice);
