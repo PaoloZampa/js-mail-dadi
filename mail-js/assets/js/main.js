3dd54db13020e0a2891aa45034eb90891556c81d
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-03-13 15:15:00 +0100

    add email function

--- mail-js/assets/js/main.js
+++ mail-js/assets/js/main.js
@@ -3,8 +3,43 @@ Chiedi all’utente la sua email, controlla che sia nella lista di chi può acce
 Mail Bonus
 Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina. */
 
-// Strummenti
-// lese if
-// for
+// Strummenti da utilizzare
+// else if
+//function
+//if else
 // dom manipulation
 // array
+
+//creo lista di mail accedere
+const emailTrue = [
+    "paolozampa@gmail.com",
+    "mattiavolpe@gmail.com",
+    "alexfioretti@gmail.com",
+    "martinaderose@gmail.com",
+    "fabiopacifici@gmail.com",
+    "porcoddue@gmail.com",
+    "vivalafesta@gmail.com",
+    "lucapiacentini@gmail.com",
+    "mariorossi@gmail.com",
+    "bigioggiero@gmail.com",
+    "alebert@gmail.com"];
+
+//function for checking if email is equal
+function emailCheck() {
+
+    //creo costanti dall'html
+    const emailUser = document.getElementById('email');
+    const message = document.getElementById('final_message')
+    const userEmail = emailUser.value;
+
+    //uso if else
+    if (emailTrue.includes(userEmail)) {
+        message.innerHTML = `L' e-mail ${userEmail} è autorizzata`;
+    } else {
+        message.innerHTML = `L' e-mail ${userEmail} non è autorizzata`;
+    }
+}
+
+//add button to check email
+const checkButton = document.getElementById('check_button');
+checkButton.addEventListener('click', emailCheck);
